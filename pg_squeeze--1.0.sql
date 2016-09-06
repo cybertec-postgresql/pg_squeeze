0224bc9c096baea7ba4dc0127aa9bbd39ff8a9c6
Antonin Houska <ah@cybertec.at>
2016-09-06 17:18:38 +0200

    Allow user to specify clustering index.

--- pg_squeeze--1.0.sql
+++ pg_squeeze--1.0.sql
@@ -3,7 +3,7 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION pg_squeeze" to load this file. \quit
 
-CREATE FUNCTION squeeze_table(relname text)
+CREATE FUNCTION squeeze_table(relation text, clustering_index text)
 RETURNS void
 AS 'MODULE_PATHNAME', 'squeeze_table'
-LANGUAGE C STRICT;
+LANGUAGE C;
