cybertec-postgresql/pg_squeeze

commit 276b244b41f8d5962993abd6b594da300eac0fb4

Adjust version check.
The previous commit should have included this.

pg_squeeze.c
@@ -56,8 +56,8 @@
 #include "utils/rel.h"
 #include "utils/syscache.h"
 
-#if PG_VERSION_NUM < 100000
-#error "PostgreSQL version 10 or higher is required"
+#if PG_VERSION_NUM < 120000
+#error "PostgreSQL version 12 or higher is required"
 #endif
 
 PG_MODULE_MAGIC;

commit 897e5a49269e0a506f3ddc47a1abecd2c8c3cfa8

Fixed array index.
Bug introduced by commit 276b244b41.

pg_squeeze.c
@@ -2711,7 +2711,7 @@ build_transient_indexes(Relation rel_dst, Relation rel_src,
 #if PG_VERSION_NUM >= 170000
 		opclassOptions = palloc0(sizeof(Datum) * ind_info->ii_NumIndexAttrs);
 		for (j = 0; j < ind_info->ii_NumIndexAttrs; j++)
-			opclassOptions[i] = get_attoptions(ind_oid, j + 1);
+			opclassOptions[j] = get_attoptions(ind_oid, j + 1);
 #endif
 
 		/*